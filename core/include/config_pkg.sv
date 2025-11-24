aba04d1dd124a883aaf94a77d65fbf4145ec6b65
yvantor <yvan.tortorella@gmail.com>
2025-11-24 16:38:24 +0100

    Integrate PULP FPU with `early_valid` feature on top.

--- core/include/config_pkg.sv
+++ core/include/config_pkg.sv
@@ -98,6 +98,8 @@ package config_pkg;
     bit                          XF16ALT;
     // Non standard 8bits Floating Point extension
     bit                          XF8;
+    // Non standard 8bits Floating Point Alt extension
+    bit                          XF8ALT;
     // Non standard Vector Floating Point extension
     bit                          XFVec;
     // Perf counters
@@ -275,6 +277,7 @@ package config_pkg;
     bit          XF16;
     bit          XF16ALT;
     bit          XF8;
+    bit          XF8ALT;
     bit          RVA;
     bit          RVB;
     bit          ZKN;
@@ -301,6 +304,7 @@ package config_pkg;
     bit          XF16Vec;
     bit          XF16ALTVec;
     bit          XF8Vec;
+    bit          XF8ALTVec;
     int unsigned NrRgprPorts;
     int unsigned NrWbPorts;
     bit          EnableAccelerator;
